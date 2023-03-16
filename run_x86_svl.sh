956f72cd5c283beab565e4eec721f7000510b74b
Amadeusz Szymko <amadeusz.szymko@put.poznan.pl>
2023-03-16 11:12:26 +0100

    chore: svl deprecated
    Signed-off-by: Amadeusz Szymko <amadeusz.szymko@put.poznan.pl>

--- run_x86_svl.sh
+++
@@ -1,7 +0,0 @@
-rocker --network host --privileged --nvidia --x11 --user --name autoware \
-  	--env="USER" \
-	--volume /dev \
-	--volume $HOME/autoware:$HOME/autoware \
-	--volume $HOME/svl \
-	--volume $HOME/.config/unity3d/LGElectronics \
-	-- ghcr.io/autowarefoundation/autoware-universe:latest-cuda
