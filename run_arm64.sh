b63a3157b29e101395f65327c8c5bf8007ddafc2
Amadeusz Szymko <amadeusz.szymko@put.poznan.pl>
2023-05-04 20:20:52 +0200

    feat: update rocker command
    Signed-off-by: Amadeusz Szymko <amadeusz.szymko@put.poznan.pl>

--- run_arm64.sh
+++ run_arm64.sh
@@ -1,6 +1,10 @@
-rocker --network host --privileged --nvidia --group-add video --x11 --user --name autoware \
+rocker --network host --privileged --nvidia runtime --group-add video --x11 --user --name autoware \
   	--env="USER" \
 	--env="RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" \
+	--env="ROS_DOMAIN_ID=0" \
+	--env="ROS_LOCALHOST_ONLY=0" \
+	--env="VEHICLE_ID=default" \
 	--volume /dev \
 	--volume $HOME/autoware:$HOME/autoware \
-	-- ghcr.io/autowarefoundation/autoware-universe:humble-latest-arm64-dev
+	--port 9090:9090 \
+	-- ghcr.io/autowarefoundation/autoware-universe:humble-latest-cuda-arm64
--- run_x86.sh
+++ run_x86.sh
@@ -1,6 +1,11 @@
 rocker --network host --privileged --nvidia --x11 --user --name autoware \
   	--env="USER" \
 	--env="RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" \
+	--env="ROS_DOMAIN_ID=0" \
+	--env="ROS_LOCALHOST_ONLY=0" \
+	--env="VEHICLE_ID=default" \
 	--volume /dev \
 	--volume $HOME/autoware:$HOME/autoware \
-	-- ghcr.io/autowarefoundation/autoware-universe:latest-cuda
+	--volume $HOME/race_common:$HOME/race_common \
+	--port 9090:9090 \
+	-- ghcr.io/autowarefoundation/autoware-universe:humble-latest-cuda-amd64
